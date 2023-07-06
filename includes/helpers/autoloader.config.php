47b91f8af98af107464ecfad3cc34e4f15584016
Utsav-Ladani <201901076@daiict.ac.in>
2023-07-06 19:11:30 +0530

    feat(app): Add the autoloader

---
+++ includes/helpers/autoloader.config.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin autoloader config
+ * Declare all constants here.
+ *
+ * @package testing-elevated
+ */
+
+namespace Testing_Elevated;
+
+/**
+ * Define constants below for specific plugin.
+ * Edit this file as per the plugin.
+ */
+
+/**
+ * Plugin namespace.
+ *
+ * @const string PLUGIN_NAMESPACE Plugin namespace.
+ */
+define( 'PLUGIN_NAMESPACE', 'Testing_Elevated' );
+
+/**
+ * Plugin directory path.
+ *
+ * @const string PLUGIN_DIR Plugin directory path.
+ */
+define( 'PLUGIN_DIR', WP_CONTENT_DIR . '/plugins/testing-elevated/' );
+

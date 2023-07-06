42556872445b110a87113dc370c2a9cfc2c7f5e9
Utsav-Ladani <201901076@daiict.ac.in>
2023-07-06 13:13:50 +0530

    feat(app): Initialize the Testing Elevated WordPress plugin

---
+++ testing-elevated.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Testing Elevated
+ * Description: Feel free to test and revert or commit the changes.
+ * Author:      Utsav Ladani
+ * Author URI:  https://profiles.wordpress.org/utsavladani/
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Version:     1.0
+ * Text Domain: testing-elevated
+ *
+ * @package testing-elevated
+ */
+
